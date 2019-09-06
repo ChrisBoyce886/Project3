1de9ea67f8a21a5163b1bcf67321577f3323a42e
Juwami <sean.nguyenf@yahoo.com>
2019-09-05 21:21:30 -0400

    fix react router and added pages for testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/pages/Home'
+import AboutUs from './components/pages/AboutUs';
+import LogIn from './components/pages/LogIn'
+import SignUp from './components/pages/SignUp'
+
 
 function App() {
 	return (
---
+++ src/components/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUp() {
+    return (
+        <h1>You Are Signing Up</h1>
+    )
+};
+
+export default SignUp
