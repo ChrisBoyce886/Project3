f3275b87d941242a0b5449c85495195249651211
Juwami <sean.nguyenf@yahoo.com>
2019-09-05 20:29:34 -0400

    add nav of login, signup, and about us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
                 <a className="navbar-brand" href="#">Earth Trek</a>
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
-                        <a className="nav-link active" href="">Link 1</a>
+                        <a className="nav-link active" href="">About Us</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="">Link 2</a>
+                        <a className="nav-link active" href="">Log In</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="">Link 3</a>
+                        <a className="nav-link active" href="">Sign Up</a>
                     </li>
                 </ul>
             </nav>
