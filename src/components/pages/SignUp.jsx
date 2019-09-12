28c138ba5cf3064d0bcb58e2db0a5299a03e6b78
Juwami <sean.nguyenf@yahoo.com>
2019-09-11 23:12:55 -0400

    add generic signup page

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 function SignUp() {
     return (
-        <h1>You Are Signing Up</h1>
+        <form className="container mt-4">
+			<div className="form-group">
+				<label for="FirstName">First Name</label>
+				<input type="text" className="form-control" id="FirstName" placeholder="First Name" />
+			</div>
+			<div className="form-group">
+				<label for="LastName">Last Name</label>
+				<input type="text" className="form-control" id="LastName" placeholder="Last Name" />
+			</div>
+			<div className="form-group">
+				<label for="Email">Email</label>
+				<input type="email" className="form-control" id="Email" placeholder="Email Address" />
+			</div>
+			<div className="form-group">
+				<label for="Password">Password</label>
+				<input type="password" className="form-control" id="Password" placeholder="Password" />
+			</div>
+			<div className="form-group">
+				<label for="RetypePassword">Retype Password</label>
+				<input type="text" className="form-control" id="PasswordRetype" placeholder="Retype Your Password" />
+			</div>
+			<button type="submit" className="btn btn-primary">
+				Submit
+			</button>
+		</form>
     )
 };
 
