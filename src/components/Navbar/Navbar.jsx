d9946a3eab74421dddd2a1b06174c8b0155b31c5
Juwami <sean.nguyenf@yahoo.com>
2019-09-11 22:38:13 -0400

    change link sizing and create generic login page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 function Navbar() {
 	return (
 		<div>
-			<nav className="navbar display-4 sticky-top navbar-dark bg-dark">
+			<nav className="navbar sticky-top navbar-dark bg-dark">
 				<a href="/" className="navbar-brand display-4">
 					<h3 className="display-4">Earth Trek</h3>
 				</a>
 				<ul className="nav justify-content-end">
 					<li className="nav-item">
 						<a href="/AboutUs/" className="nav-link active">
-							About Us
+							<h4>About Us</h4>
 						</a>
 					</li>
 					<li className="nav-item">
 						<a href="/LogIn/" className="nav-link active">
-							Log In
+							<h4>Log In</h4>
 						</a>
 					</li>
 					<li className="nav-item">
 						<a href="/SignUp/" className="nav-link active">
-							Sign Up
+							<h4>Sign Up</h4>
 						</a>
 					</li>
 				</ul>
--- src/components/pages/LogIn.jsx
+++ src/components/pages/LogIn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function LogIn() {
 	return (
-		<form>
+		<form className="container mt-4">
 			<div className="form-group">
 				<label for="Username">Username</label>
 				<input type="text" className="form-control" id="Username" placeholder="Enter Username" />
