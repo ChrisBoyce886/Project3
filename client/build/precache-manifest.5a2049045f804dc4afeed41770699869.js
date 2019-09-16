1fd48c598b4d331bfabf01599bb9bc3a4299836f
Juwami <sean.nguyenf@yahoo.com>
2019-09-15 22:47:38 -0400

    create build folder from script and also seedDB.js for data load

---
+++ client/build/precache-manifest.5a2049045f804dc4afeed41770699869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc075fa363835cbba50a996e234f2d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6390b7d7ba8ce699bf2c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bba2e33b5cc0bf686ea9",
+    "url": "/static/js/2.c322e645.chunk.js"
+  },
+  {
+    "revision": "6390b7d7ba8ce699bf2c",
+    "url": "/static/js/main.2d7dbaf6.chunk.js"
+  },
+  {
+    "revision": "d06e4877cb1725d89864",
+    "url": "/static/js/runtime~main.38f12057.js"
+  }
+]);
