9923bd0ce9e6aae2256bc4b514c8d595ae9a7bc7
Juwami <sean.nguyenf@yahoo.com>
2019-09-16 20:33:56 -0400

    center search bar using d-flex still having issues with viewport

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,8 +3,8 @@ import SubmitBtn from '../SubmitBtn/SubmitBtn';
 
 const SearchBar = () => {
 	return (
-		<div className="form-group border">
-			<label htmlFor="search">Where would you like to hike?</label>
+		<div className="form-group vh-100 d-flex align-items-center m-0">
+			<label htmlFor="search" className="d-flex">Where would you like to hike?</label>
 			<div class="container d-flex">
 				<input className="form-control mr-2" name="search" />
 				<SubmitBtn/>
