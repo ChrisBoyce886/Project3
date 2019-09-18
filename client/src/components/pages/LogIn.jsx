473d7fe1cdc5785a03f8ef5cae87c1cf2c79f972
Juwami <sean.nguyenf@yahoo.com>
2019-09-17 20:02:28 -0400

    turn sign up and login into class components as I need to pull in values from the user

--- client/src/components/pages/LogIn.jsx
+++ client/src/components/pages/LogIn.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function LogIn() {
-	return (
-		<form className="container mt-4">
-			<div className="form-group">
-				<label for="Username">Username</label>
-				<input type="text" className="form-control" id="Username" placeholder="Enter Username" />
-			</div>
-			<div className="form-group">
-				<label for="Password">Password</label>
-				<input type="password" className="form-control" id="Password" placeholder="Password" />
-			</div>
-			<button type="submit" className="btn btn-primary">
-				Submit
-			</button>
-		</form>
-	);
+class LogIn extends Component {
+	state = {
+		username: '',
+		password: ''
+	}
+	render() {
+		return (
+			<form className="container mt-4">
+				<div className="form-group">
+					<label for="Username">Username</label>
+					<input type="text" className="form-control" id="Username" placeholder="Enter Username" />
+				</div>
+				<div className="form-group">
+					<label for="Password">Password</label>
+					<input type="password" className="form-control" id="Password" placeholder="Password" />
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		);
+	}
 }
 
 export default LogIn;
--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function SignUp() {
-    return (
-        <form className="container mt-4">
-			<div className="form-group">
-				<label for="FirstName">First Name</label>
-				<input type="text" className="form-control" id="FirstName" placeholder="First Name" />
-			</div>
-			<div className="form-group">
-				<label for="LastName">Last Name</label>
-				<input type="text" className="form-control" id="LastName" placeholder="Last Name" />
-			</div>
-			<div className="form-group">
-				<label for="Email">Email</label>
-				<input type="email" className="form-control" id="Email" placeholder="Email Address" />
-			</div>
-			<div className="form-group">
-				<label for="Password">Password</label>
-				<input type="password" className="form-control" id="Password" placeholder="Password" />
-			</div>
-			<div className="form-group">
-				<label for="RetypePassword">Retype Password</label>
-				<input type="text" className="form-control" id="PasswordRetype" placeholder="Retype Your Password" />
-			</div>
-			<button type="submit" className="btn btn-primary">
-				Submit
-			</button>
-		</form>
-    )
-};
+class SignUp extends Component {
+	state = {
+		firstname: '',
+		lastname: '',
+		email: '',
+		password: '',
+		retypedpassword: '',
+	};
+	render() {
+		return (
+			<form className="container mt-4">
+				<div className="form-group">
+					<label htmlFor="FirstName">First Name</label>
+					<input type="text" className="form-control" id="FirstName" placeholder="First Name" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="LastName">Last Name</label>
+					<input type="text" className="form-control" id="LastName" placeholder="Last Name" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="Email">Email</label>
+					<input type="email" className="form-control" id="Email" placeholder="Email Address" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="Password">Password</label>
+					<input type="password" className="form-control" id="Password" placeholder="Password" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="RetypePassword">Retype Password</label>
+					<input
+						type="text"
+						className="form-control"
+						id="PasswordRetype"
+						placeholder="Retype Your Password"
+					/>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		);
+	}
+}
 
-export default SignUp
+export default SignUp;
