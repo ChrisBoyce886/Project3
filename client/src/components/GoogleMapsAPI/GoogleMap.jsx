8ec2755b66f5df963aa2cc77e745669f2aec63e5
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-09-21 10:28:07 -0400

    Re-Commit last

--- client/src/components/GoogleMapsAPI/GoogleMap.jsx
+++ client/src/components/GoogleMapsAPI/GoogleMap.jsx
@@ -45,7 +45,7 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GOOGLEMAPSAPIKEY
+  apiKey: process.env.REACT_APP_GOOGLEMAPSAPIKEY 
 })(MapContainer);
 
 
