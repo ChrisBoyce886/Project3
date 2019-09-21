6f65adb36a77ce8fa44c364c0ac9d096048eaca9
Juwami <sean.nguyenf@yahoo.com>
2019-09-21 02:13:54 -0400

    add skeleton code for user authentication and user create

--- server/routes/api/UserAPI.js
+++ server/routes/api/UserAPI.js
@@ -1,3 +1,25 @@
-const axios = require('axios');
-const router = require('express').Router();
+let db = require("../../models");
+let passport = require("passport");
 
+module.exports = function (app) {
+
+    // Post: Authenticate user
+    app.post("/user/authenticate",
+    passport.authenticate("local", {
+        successRedirect: "/userProfile",
+        failureRedirect: "/"
+    }));
+
+    // Post : Add User
+    app.post("/user/create", (req, res) => {
+        console.log(req.body)
+        db.User.create({
+
+        })
+    })
+    .then( () => {
+        res.send(200).end();
+    }).catch( (err) => {
+        res.status(401).json(err)
+    })
+}
