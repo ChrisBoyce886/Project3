bb41f365ffbec708ddf00c79239096ec7baa74c8
Juwami <sean.nguyenf@yahoo.com>
2019-09-22 22:27:49 -0400

    attempt to fix routes

--- client/src/utils/CredentialsAPI.js
+++ client/src/utils/CredentialsAPI.js
@@ -12,9 +12,10 @@ export default {
     AuthenticateUser: function (info) {
         return axios.post('/user/authenticate', info)
         .then(res => {
-            console.log(res.data)
-            let token = res.data
-            localStorage.setItem("authToken", token)
+            // console.log(res.data)
+            // let token = res.data
+            // console.log(token)
+            return window.localStorage.setItem("authToken", res.data)
         })
     }
 }
