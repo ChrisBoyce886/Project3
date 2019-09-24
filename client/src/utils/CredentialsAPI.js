1c7b3749d5289447b972089f45d6838d9c1eba4a
Juwami <sean.nguyenf@yahoo.com>
2019-09-24 17:59:54 -0400

    received assistance in pulling token data

--- client/src/utils/CredentialsAPI.js
+++ client/src/utils/CredentialsAPI.js
@@ -1,21 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    CreateNewUser: function (info) {
-        return axios.post('/user/create', info)
-        .then(res => {
-            // res.data.token <<<<        
-            console.log(res)
-        })
-    },
+	CreateNewUser: function(info) {
+		return axios.post('/user/create', info).then((res) => {
+			// res.data.token <<<<
+			console.log(res);
+		});
+	},
 
-    AuthenticateUser: function (info) {
-        return axios.post('/user/authenticate', info)
-        .then(res => {
-            // console.log(res.data)
-            // let token = res.data
-            // console.log(token)
-            return window.localStorage.setItem("authToken", res.data)
-        })
-    }
-}
+	AuthenticateUser: function(info) {
+		return axios.post('/user/authenticate', info)
+			// let token = res.data
+			// console.log(token)
+			// window.localStorage.setItem('authToken', res.data);
+	}
+};
