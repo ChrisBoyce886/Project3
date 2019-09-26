6a72349950d822c38138dbfdaef46dfff9e68d99
Juwami <sean.nguyenf@yahoo.com>
2019-09-26 19:24:30 -0400

    create components for columns and add to userprofile using bootstrap

---
+++ client/src/components/Done_Column/Done_Column.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Done_Column = (props) => {
+	return <div>
+		<h3 className="text-center">Done</h3>
+	</div>;
+};
+
+export default Done_Column;
---
+++ client/src/components/Favorite_Column/Favorite_Column.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Favorite_Column = (props) => {
+    return <div>
+        <h3 className="text-center">Favorites</h3>
+    </div>
+}
+
+export default Favorite_Column
